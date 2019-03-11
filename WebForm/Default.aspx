b691850a33c57c7b2e09056252d474fb349ec2d5
nhan98453 <nhan98453@gmail.com>
2019-03-11 17:59:13 +0700

    Thêm webform

---
+++ WebForm/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebForm._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <form id="frmProduct">
+            <div class="col-sm-12">
+                <table class="table table-bordered" id="tblProduct" >
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Tên thiết bị</th>
+                            <th>Loại thiết bị</th>
+                            <th>Đơn giá</th>
+                            <th>Số lượng tồn</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tbl_ThietBi" runat="server"></tbody>
+                </table>
+            </div>
+        </form>
+
+</asp:Content>
---
+++ WebForm/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using BUS;
+using DTO;
+namespace WebForm
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<ThietBi> tb = ThietBiBUS.ListTB();
+            foreach (var item in tb)
+            {
+                HtmlTableRow row = new HtmlTableRow();
+                HtmlTableCell cell = new HtmlTableCell();
+                cell.InnerText = item.Matb;
+                HtmlTableCell cell1 = new HtmlTableCell();
+                cell1.InnerText = item.Tentb;
+                HtmlTableCell cell2 = new HtmlTableCell();
+                cell2.InnerText = item.Loaitb;
+
+                HtmlTableCell cell3 = new HtmlTableCell();
+                cell3.InnerText = item.Dongia.ToString();
+                HtmlTableCell cell4 = new HtmlTableCell();
+                cell4.InnerText = item.SL.ToString();
+                row.Controls.Add(cell);
+                row.Controls.Add(cell1);
+                row.Controls.Add(cell2);
+                row.Controls.Add(cell3);
+                row.Controls.Add(cell4);
+                tbl_ThietBi.Controls.Add(row);
+
+            }
+        }
+    }
+}
